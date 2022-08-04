9a99219cb697e4d8967583bbc6654814f39d7cbb
lkoWilliam41 <elokosama@gmail.com>
2022-08-04 22:19:25 +0200

    la position du bouton connexion reajuster sur la nav format mobile

--- element.js
+++ element.js
@@ -56,7 +56,7 @@ var topbar = `<header>
                                     </li>
                                     <li><a href="apropos.html" class="menu">A propos de Nous</a></li>
                                     <li><a href="contact.html" class="menu">Contactez Nous</a></li>
-                                    <button class="n btn-outline-primary">Connexion</button>
+                                    <button style="margin-left: 15px;" class="n btn-outline-primary menu">Connexion</button>
                                 </ul>
                             </nav>
                         </div>
