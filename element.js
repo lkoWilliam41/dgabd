08df29bb8129c4c8199fade694a189656e697881
Kevine-P <manimapierre31@gmail.com>
2022-08-05 01:52:00 +0100

    ajout 01h51

--- element.js
+++ element.js
@@ -36,8 +36,8 @@ var topbar = `<header>
                 <div class="row align-items-center">
                     <!-- Logo -->
                     <div class="col-xl-2 col-lg-2 col-md-1">
-                        <div class="logo">
-                            <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                        <div class="log">
+                            <a href="index.html"><img  src="assets/img/logo/logo.png" alt=""></a>
                         </div>
                     </div>
                     <div class="col-xl-10 col-lg-10 col-md-10">
@@ -82,7 +82,7 @@ var bas = ` <footer>
                 <div class="single-footer-caption mb-50">
                     <div class="single-footer-caption mb-30">
                         <!-- logo -->
-                        <div class="footer-logo">
+                        <div class="footer-log">
                             <a href="index.html"><img src="assets/img/logo/logo2_footer.png" alt=""></a>
                         </div>
                         <div class="footer-tittle">
